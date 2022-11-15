6db078dbaef202f0e8968c4b1f1857f6bcec27ff
Crowdbotics <team@crowdbotics.com>
2022-11-15 18:49:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "quik_payment_37597",
+  url: "https://quik_payment_37597.botics.co",
+  api: "https://quik_payment_37597.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
